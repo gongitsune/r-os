74d70e2a3f11d6eacdc2e07daacd71ce09e3d968
kara <89068705+gongitsune@users.noreply.github.com>
2023-11-25 19:26:36 +0900

    feat: load kernel

--- devenv/run_qemu.sh
+++ devenv/run_qemu.sh
@@ -5,8 +5,8 @@ LIGHT_BLUE='\033[1;34m'
 RED='\033[0;31m'
 RESET='\033[0m'
 
-exec 2> >(awk '{print strftime("%H:%M:%S ['${RED}'ERROR'${RESET}'] ") $0;  system("");}')
-exec 1> >(awk '{print strftime("%H:%M:%S ['${LIGHT_BLUE}'INFO'${RESET}'] ") $0; system("");}')
+# exec 2> >(awk '{print strftime("%H:%M:%S ['${RED}'ERROR'${RESET}'] ") $0;  system("");}')
+# exec 1> >(awk '{print strftime("%H:%M:%S ['${LIGHT_BLUE}'INFO'${RESET}'] ") $0; system("");}')
 
 # Set up variables
 DEVENV_DIR=$(dirname "$0")
--- loader/src/main.rs
+++ loader/src/main.rs
@@ -7,12 +7,15 @@ extern crate alloc;
 use core::mem;
 
 use alloc::format;
+use byteorder::{ByteOrder, LittleEndian};
 use log::info;
 use uefi::{
     prelude::*,
-    proto::media::file::{File, FileAttribute, FileMode},
-    table::{boot::MemoryDescriptor, runtime::ResetType},
+    proto::media::file::{File, FileAttribute, FileInfo, FileMode},
+    table::boot::{AllocateType, MemoryDescriptor, MemoryType},
 };
+
+const KERNEL_BASE_ADDR: u64 = 0x100000;
 
 #[entry]
 fn efi_main(handle: Handle, mut st: SystemTable<Boot>) -> Status {
@@ -20,11 +23,62 @@ fn efi_main(handle: Handle, mut st: SystemTable<Boot>) -> Status {
     st.stdout().reset(false).unwrap();
 
     dump_mem_map(handle, &st);
+    load_kernel(handle, &st);
 
-    st.boot_services().stall(3_000_000);
-    st.stdout().reset(false).unwrap();
-    st.runtime_services()
-        .reset(ResetType::SHUTDOWN, Status::SUCCESS, None);
+    // stop boot service
+    start_kernel(st);
+
+    Status::SUCCESS
+}
+
+fn start_kernel(st: SystemTable<Boot>) {
+    info!("start kernel");
+
+    let kernel_entry_addr = LittleEndian::read_u64(unsafe {
+        core::slice::from_raw_parts((KERNEL_BASE_ADDR + 24) as *const u8, mem::size_of::<u64>())
+    });
+    info!("kernel_entry_addr: {:p}", kernel_entry_addr as *const ());
+
+    let _ = st.exit_boot_services(MemoryType::LOADER_DATA);
+
+    let kernel_entry: extern "sysv64" fn() = unsafe { mem::transmute(kernel_entry_addr) };
+    kernel_entry();
+
+    panic!("kernel returned");
+}
+
+fn load_kernel(handle: Handle, st: &SystemTable<Boot>) {
+    info!("load kernel");
+
+    let bt = st.boot_services();
+    let mut fs = bt.get_image_file_system(handle).unwrap();
+
+    let mut root_dir = fs.open_volume().unwrap();
+    let mut elf_file = root_dir
+        .open(
+            cstr16!("kernel.elf"),
+            FileMode::Read,
+            FileAttribute::empty(),
+        )
+        .unwrap()
+        .into_regular_file()
+        .unwrap();
+
+    let file_info = elf_file.get_boxed_info::<FileInfo>().unwrap();
+    let size = file_info.file_size() as usize;
+
+    bt.allocate_pages(
+        AllocateType::Address(KERNEL_BASE_ADDR),
+        MemoryType::LOADER_DATA,
+        (size as usize + 0xfff) / 0x1000,
+    )
+    .unwrap();
+
+    elf_file
+        .read(unsafe { core::slice::from_raw_parts_mut(KERNEL_BASE_ADDR as *mut u8, size) })
+        .unwrap();
+
+    info!("done");
 }
 
 fn dump_mem_map(handle: Handle, st: &SystemTable<Boot>) {
